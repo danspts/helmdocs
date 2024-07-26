5044ecc81d2a95b02a4efcd8dd34c6b9701497a3
danspts <danspts@gmail.com>
2024-07-26 14:15:35 +0300

    CLI - add basic cli functions and git init

---
+++ cmd/cmd.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'generate' subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "generate":
+		generateCmd := flag.NewFlagSet("generate", flag.ExitOnError)
+		generateCmd.Usage = func() {
+			fmt.Fprintf(generateCmd.Output(), "Usage of %s:\n", "generate")
+			fmt.Println("  readme      Generates README")
+			fmt.Println("  values      Generates values")
+			generateCmd.PrintDefaults()
+		}
+
+		if len(os.Args) < 3 {
+			fmt.Println("expected 'readme' or 'values' subcommand")
+			generateCmd.Usage()
+			os.Exit(0)
+		}
+
+		command := "\033[1mhelmdocs\033[0m "
+		command += strings.Join(os.Args[1:3], " ")
+		switch os.Args[2] {
+		case "readme":
+			handleGenerateReadmeCommand(command, os.Args[3:])
+		case "values":
+			handleGenerateValuesCommand(command, os.Args[3:])
+		default:
+			fmt.Println("expected 'readme' or 'values' subcommand")
+			generateCmd.Usage()
+			os.Exit(1)
+		}
+	default:
+		fmt.Println("expected 'generate' subcommand")
+		os.Exit(1)
+	}
+}
+
+func usage(command string, f *flag.FlagSet) func() {
+	return func() {
+		fmt.Fprintf(f.Output(), "Usage of %s:\n", command)
+		f.PrintDefaults()
+	}
+}
+
+func handleGenerateReadmeCommand(command string, args []string) {
+	generateReadmeCmd := flag.NewFlagSet("readme", flag.ExitOnError)
+	schemaPath := generateReadmeCmd.String("schema-path", "values.schema.json", "Path to the schema file")
+
+	generateReadmeCmd.Usage = usage(command, generateReadmeCmd)
+
+	generateReadmeCmd.Parse(args)
+	generateReadme(*schemaPath)
+}
+
+func handleGenerateValuesCommand(command string, args []string) {
+	generateValuesCmd := flag.NewFlagSet("values", flag.ExitOnError)
+	schemaPath := generateValuesCmd.String("schema-path", "values.schema.json", "Path to the schema file")
+	omitDefault := generateValuesCmd.Bool("omit-default", false, "Omit default values")
+
+	generateValuesCmd.Usage = usage(command, generateValuesCmd)
+
+	generateValuesCmd.Parse(args)
+	generateValues(*schemaPath, *omitDefault)
+}
+
+func generateReadme(schemaPath string) {
+	fmt.Printf("Generating README with schema path: %s\n", schemaPath)
+	// Add logic to generate README based on the schemaPath
+}
+
+func generateValues(schemaPath string, omitDefault bool) {
+	fmt.Printf("Generating values with schema path: %s, omit default: %t\n", schemaPath, omitDefault)
+	// Add logic to generate values based on the schemaPath and omitDefault
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danspts/helmdocs
+
+go 1.22.1
